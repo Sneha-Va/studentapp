2e27838cdbc72b3432dc3ffebe6c9dbe35a26e2f
Snrha-Va <sneha.varghese@nestgroup.net>
2022-11-01 16:23:33 +0530

    first command

---
+++ student.py
@@ -0,0 +1,21 @@
+while True:
+    print("select an option from the menu")
+    print("1 add student")
+    print ("2 view all student")
+    print("3 search a student")
+    print("4 update the student")
+    print("5 delete the student")
+    print("6 exit")
+    choice=int(input('enter the option:'))
+    if (choice==1):
+        print("student enter selectrd")
+    elif (choice==2):
+        print("view all student")
+    elif (choice==3):
+      print("searchall student")
+    elif (choice==4):
+         print("update all student")
+    elif (choice==5):
+        print("delete all student")
+    elif (choice==6):  
+        break
